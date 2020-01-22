36b2c5c7241d73a36b18215a607dba5409cddd9f
Jacob Robinson <95jacob07@gmail.com>
2020-01-22 11:43:50 -0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <ul>
+      <li v-for="team in match.teams"> 
+        
+      </li>
+    </ul> -->
+    <button v-on:click="stop" v-bind:disabled="!shuffling" class="stop-btn">
+      <h1>{{ match.pressYourLuck.teams[active].name }}</h1>
+      <h3>Spins Left: {{ match.pressYourLuck.teams[active].spins }}</h3>
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import openSocket from "socket.io-client";
+import MatchService from "./MatchService";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: function() {
+    return {
+      match: {},
+      active: 0,
+      shuffling: false
+    };
+  },
+  methods: {
+    async stop() {
+      try {
+        const result = await MatchService.stop();
+        // eslint-disable-next-line no-console
+        console.log(result);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      }
+    }
+  },
+  async created() {
+    try {
+      this.match = await MatchService.getMatch();
+      this.active = this.match.pressYourLuck.activeTeam;
+      // const socket = openSocket("https://game-show.herokuapp.com");
+      const socket = openSocket("http://localhost:8000");
+      socket.on("pressYourLuck", data => {
+        const { action } = data;
+        if (action === "start") {
+          const { spins } = data;
+          this.match.pressYourLuck.teams[
+            this.match.pressYourLuck.activeTeam
+          ].spins = spins;
+          this.shuffling = true;
+        }
+        if (action == "stop") {
+          if (this.shuffling) {
+            this.shuffling = false;
+          }
+        }
+        if (action === "generalUpdate") {
+          // eslint-disable-next-line no-console
+          console.log("im here");
+          const { pressYourLuck } = data;
+          this.match.pressYourLuck = pressYourLuck;
+          this.active = this.match.pressYourLuck.activeTeam;
+        }
+      });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+    }
   }
-}
+};
 </script>
 
 <style>
+body {
+  background: #333;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #252525;
+}
+.stop-btn {
+  height: 90vh;
+  width: 90vw;
+  margin: 5vh auto;
+  background: #555;
+
+  -moz-box-shadow: inset 0 0 10px #000000;
+  -webkit-box-shadow: inset 0 0 10px #000000;
+  box-shadow: inset 0 0 40px #000000;
+  border: solid 3px #000000aa;
+}
+
+.stop-btn:disabled {
+  background: #333;
 }
 </style>
---
+++ src/MatchService.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+// const url = "https://game-show.herokuapp.com/match/";
+const url = "http://localhost:8000/match/";
+
+// const pressYourLuckURL = "https://game-show.herokuapp.com/press-your-luck/";
+const pressYourLuckURL = "http://localhost:8000/press-your-luck/";
+
+class MatchService {
+  static getMatch() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        resolve(data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  static stop() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.post(pressYourLuckURL + "stop");
+        const data = res.data;
+        resolve(data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+}
+
+export default MatchService;
